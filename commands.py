c4f8d80712385652a3584a8610b307bb639fef0b
coffincw <coffincw@gmail.com>
2019-04-03 22:30:39 -0400

    set up help command

--- commands.py
+++ commands.py
@@ -22,6 +22,45 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+@client.command(name='help', pass_context=True)
+async def help(ctx):
+    """help command"""
+    if ctx.message.author.server_permissions.administrator:
+        # obtain the argument (remove the command and the leading and trailing whitespaces)
+        argument = ctx.message.content[7:].strip()
+        print(argument == 'create')
+        if argument == 'create':
+            await helpcreate(ctx)
+            return
+        embed = discord.Embed(color = discord.Color.orange())
+        embed.set_author(name='Help', icon_url='https://cdn.discordapp.com/attachments/561393414571294752/563071668533067901/announcement_icon.jpg')
+        embed.add_field(
+            name='Commands', 
+            value='*e!create [text]* - Creates an an embedded post, to learn more about create try e!help create\n*e!preview* - Sends a preview of the announcement to the requesting channel\n*e!channel [#channel]* - Sets the announcement channel\n*e!post* - post the announcement to the set channel', 
+            inline=True
+        )
+        await client.say(embed=embed)
+
+async def helpcreate(ctx):
+    embed = discord.Embed(color=discord.Color.orange())
+    embed.set_author(name='Help | Create', icon_url='https://cdn.discordapp.com/attachments/561393414571294752/563071668533067901/announcement_icon.jpg')
+    embed.add_field(
+        name='Title',
+        value='The first part of your input is the title of the announcement, | should be put at the end of the title and any field.\nIn the input: `e!create Contest | Details<This is a test` The title is `Contest`',
+        inline=True
+    )
+    embed.add_field(
+        name='Sections',
+        value='Separate sections with |',
+        inline=True
+    )
+    embed.add_field(
+        name='Section Headers',
+        value='^This is a section header, section headers are the first few words after a | before a < in inputed\nIn the input: `e!create Contest | Other Details<The event will take place...` Other Details would be the header',
+        inline=True
+    )
+    await client.send_message(ctx.message.channel, embed=embed)
+
 @client.command(name='create', pass_context=True)
 async def create(ctx):
     """The bot creates an announcement and saves it for the server.  The user can then preview or post the announcement
@@ -29,38 +68,39 @@ async def create(ctx):
     Args:
         - ctx: context of the command
     """
-    # get the input (strip the command syntax)
-    message = ctx.message.content[9:]
+    if ctx.message.author.server_permissions.administrator:
+        # get the input (strip the command syntax)
+        message = ctx.message.content[9:]
 
-    # split message based on '|'
-    input = message.split('|')
-    
-    if len(input) is 0:
-        await client.send_message(ctx.message.channel, embed=discord.Embed(description='No contents specified!', color=discord.Color.red()))
-        return
+        # split message based on '|'
+        input = message.split('|')
+        
+        if len(input) is 0:
+            await client.send_message(ctx.message.channel, embed=discord.Embed(description='No contents specified!', color=discord.Color.red()))
+            return
 
-    # create embed with teal color
-    announcement = discord.Embed(color=discord.Color.teal())
-    
-    # set author based on the first part of the users message 
-    announcement.set_author(name=input[0], icon_url='https://cdn.discordapp.com/attachments/561393414571294752/563071668533067901/announcement_icon.jpg')
-    input = input[1:]
-    for segment in input:
-        # use the first word as the field name
-        field_name = segment.split(' ', 1)[0]
+        # create embed with teal color
+        announcement = discord.Embed(color=discord.Color.teal())
+        
+        # set author based on the first part of the users message 
+        announcement.set_author(name=input[0], icon_url='https://cdn.discordapp.com/attachments/561393414571294752/563071668533067901/announcement_icon.jpg')
+        input = input[1:]
+        for segment in input:
+            # use the first word as the field name
+            field_name = segment.split('<', 1)[0]
 
-        # use the rest as the value
-        field_text = segment.split(' ', 1)[1]
+            # use the rest as the value
+            field_text = segment.split('<', 1)[1]
 
-        #add the field
-        announcement.add_field(
-            name=field_name,
-            value=field_text,
-            inline=True
-        )
-    # set announcement
-    current_announcement[ctx.message.server.id] = announcement
-    await client.send_message(ctx.message.channel, embed=discord.Embed(description='Announcement set! Preview with with e!preview', color=discord.Color.green()))
+            #add the field
+            announcement.add_field(
+                name=field_name,
+                value=field_text,
+                inline=True
+            )
+        # set announcement
+        current_announcement[ctx.message.server.id] = announcement
+        await client.send_message(ctx.message.channel, embed=discord.Embed(description='Announcement set! Preview with with e!preview', color=discord.Color.green()))
 
 
 
@@ -71,17 +111,18 @@ async def set_announcement_channel(ctx):
     Args:
         - ctx: context of the command
     """
-    # gets the channel id from form: <#0000000000> to 00000000000
-    channel_id = ctx.message.content[10:].replace(" ", "").replace("<", "").replace("#", "").replace(">", "")
+    if ctx.message.author.server_permissions.administrator:
+        # gets the channel id from form: <#0000000000> to 00000000000
+        channel_id = ctx.message.content[10:].replace(" ", "").replace("<", "").replace("#", "").replace(">", "")
 
-    # get the selected channel
-    channel = client.get_channel(channel_id)
-    if channel is None:
-        await client.send_message(ctx.message.channel, embed=discord.Embed(description="Invalid channel", color=discord.Color.red()))
-    else:
-        #set announcement channel for current server
-        announcement_channel[ctx.message.server.id] = channel
-        await client.send_message(ctx.message.channel, embed=discord.Embed(description="Set announcements channel as " + channel.mention, color=discord.Color.green()))
+        # get the selected channel
+        channel = client.get_channel(channel_id)
+        if channel is None:
+            await client.send_message(ctx.message.channel, embed=discord.Embed(description="Invalid channel", color=discord.Color.red()))
+        else:
+            #set announcement channel for current server
+            announcement_channel[ctx.message.server.id] = channel
+            await client.send_message(ctx.message.channel, embed=discord.Embed(description="Set announcements channel as " + channel.mention, color=discord.Color.green()))
 
 @client.command(name='preview', pass_context=True)
 async def preview(ctx):
@@ -90,12 +131,13 @@ async def preview(ctx):
     Args:
         - ctx: context of the command (used for getting the server id) 
     """
-    # get the servers announcement
-    announcement = current_announcement.get(ctx.message.server.id)
-    if announcement is None:
-        await client.send_message(ctx.message.channel, embed=discord.Embed(description="No announcement message set!", color=discord.Color.red()))
-    else:
-        await client.send_message(ctx.message.channel, embed=announcement)
+    if ctx.message.author.server_permissions.administrator:
+        # get the servers announcement
+        announcement = current_announcement.get(ctx.message.server.id)
+        if announcement is None:
+            await client.send_message(ctx.message.channel, embed=discord.Embed(description="No announcement message set! Set an announcement with e!create", color=discord.Color.red()))
+        else:
+            await client.send_message(ctx.message.channel, embed=announcement)
 
 @client.command(name='post', pass_context=True)
 async def post(ctx):
@@ -104,16 +146,19 @@ async def post(ctx):
     Args:
         - ctx: context of the command (used for getting the server id) 
     """
-    # get server announcement
-    announcement = current_announcement.get(ctx.message.server.id)
-    if announcement is None: # if no announcement set
-        await client.send_message(ctx.message.channel, embed=discord.Embed(description="No announcement message set!", color=discord.Color.red()))
-        return
-    # get announcement channel
-    channel = announcement_channel.get(ctx.message.server.id)
-    if channel is None: # if no announcement channel set
-        await client.send_message(ctx.message.channel, embed=discord.Embed(description="No announcement channel set!", color=discord.Color.red()))
-    else:
-        await client.send_message(channel, embed=announcement)
+    if ctx.message.author.server_permissions.administrator:
+        # get server announcement
+        announcement = current_announcement.get(ctx.message.server.id)
+        if announcement is None: # if no announcement set
+            await client.send_message(ctx.message.channel, embed=discord.Embed(description="No announcement message set! Set an announcement with e!create", color=discord.Color.red()))
+            return
+        # get announcement channel
+        channel = announcement_channel.get(ctx.message.server.id)
+        if channel is None: # if no announcement channel set
+            await client.send_message(ctx.message.channel, embed=discord.Embed(description="No announcement channel set! Set an announcement channel with e!channel", color=discord.Color.red()))
+        else:
+            everyone = ctx.message.server.roles[0]
+            await client.send_message(channel, everyone)
+            await client.send_message(channel, embed=announcement)
 
 client.run(BOT_TOKEN)
